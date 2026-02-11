ce79bf09a5cd72166ac68b32df9fe57f7d4300eb
AzureAD\VeronicaPardo <veronica.pardo@aelis.es>
2026-02-11 12:26:15 +0100

    Rediseño en Horizontal del inicio

--- app/src/main/java/com/example/tfg/adapter/CitasAdapter.kt
+++ app/src/main/java/com/example/tfg/adapter/CitasAdapter.kt
@@ -29,7 +29,6 @@ class CitasAdapter(
 
             tvNombreItem.text = "${contexto.getString(R.string.nombre_cliente)}: ${cita.nombre}"
             tvServicioItem.text = "${contexto.getString(R.string.servicio)}: ${cita.servicio}"
-            tvEstadoItem?.text = "${contexto.getString(R.string.fechaDef)}: ${cita.fecha}"
             tvFechaItem.text = "${contexto.getString(R.string.horaDef)}: ${cita.hora}"
             root.setOnClickListener { onCitaClick(cita) }
         }
